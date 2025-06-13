b77a709dfeaa0b0dce511679518efae306205f80
Anthasss <anthasprog@gmail.com>
2025-06-13 21:05:40 +0800

    commit duu

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   timeout: 10000,
 });
